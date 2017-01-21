5c34f374d4a0743d95a4312e7a28a9c0801358b7
wenqian <wenqian@123.com>
2017-01-21 21:27:49 +0800

    111

--- containers/head.jsx
+++ containers/head.jsx
@@ -28,17 +28,19 @@ export class Head extends React.Component {
     };
   }
   handleClickAll(){
-    this.state.selectAll=!this.state.selectAll;
+    // this.state.selectAll=!this.state.selectAll;//不能用这种方法，要用下面的setState来更改状态
+    this.setState(Object.assign({},this.state,{selectAll:!this.state.selectAll}));
     console.log('clickAll:'+this.state.selectAll);
   }
   handleClickDelete(){
-    this.state.deleteThis=!this.state.deleteThis;
+    // this.state.deleteThis=!this.state.deleteThis;
+    this.setState(Object.assign({},this.state,{deleteThis:!this.state.deleteThis}));
     console.log('clickdelete:'+this.state.deleteThis);
   }
   render(){
     return (<div className="myhead">
-    <span onClick={()=>{this.handleClickAll()}} className={this.state.selectAll?"checked":"unchecked"}>全选</span>{/*这里的绑定不能用this.handleClickAll.bind(this),提示没有bind is undefined*/}
-    <span onClick={()=>{this.handleClickDelete()}} className={this.state.deleteThis?"checked":"unchecked"}>删除</span>
+    <span onClick={()=>{this.handleClickAll()}} className={this.state.selectAll?"checked":"unchecked"}>全选</span>{/*这里的绑定用箭头函数()=>{this.handleClickAll()}，而用this.handleClickAll.bind(this),提示bind is undefined？？？后来全都换成箭头函数，就不出错，然后又全都换成bind又都正常，不报错了。诡异！*/}
+    <span onClick={this.handleClickDelete.bind(this)} className={this.state.deleteThis?"checked":"unchecked"}>删除</span>
     </div>);
   }
 }
