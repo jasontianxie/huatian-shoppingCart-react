d70b433cf89b5e9e3037d22341e7e9399cb36185
wenqian <wenqian@123.com>
2017-02-07 21:53:30 +0800

    react-redux

--- containers/head.jsx
+++ containers/head.jsx
@@ -18,32 +18,59 @@
 //     </div>);
 //   }
 // });
+import { connect } from 'react-redux';//redux(使用 react-redux)
+import { Provider } from 'react-redux';//redux(使用 react-redux)
 import * as React from 'react';//注意：如果该组件用到了React变量，则组件中要引入React，否则报错。from后面的路径，可以直接写模块名，则默认会自动查找node_modules中的相关模块，如果不是直接写的模块名，则要写成相对路径或者绝对路径。相对路径本文件夹用./表示。上一级文件夹用../表示
-export class Head extends React.Component {
+class Head extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      selectAll: false,
-      deleteThis: false
-    };
+    // this.state = {
+    //   selectAll: false,
+    //   deleteThis: false
+    // };
   }//用redux后，状态都写在了store中，这里就不用state了。
-  handleClickAll(){//没有用redux时使用的方法
+  handleClickAll() {//没有用redux时使用的方法
     // this.state.selectAll=!this.state.selectAll;//不能用这种方法，要用下面的setState来更改状态
-    this.setState(Object.assign({},this.state,{selectAll:!this.state.selectAll}));
-    console.log('clickAll:'+this.state.selectAll);
+    this.setState(Object.assign({}, this.state, { selectAll: !this.state.selectAll }));
+    console.log('clickAll:' + this.state.selectAll);
   }
-  handleClickDelete(){//没有用redux时使用的方法
+  handleClickDelete() {//没有用redux时使用的方法
     // this.state.deleteThis=!this.state.deleteThis;
-    this.setState(Object.assign({},this.state,{deleteThis:!this.state.deleteThis}));
-    console.log('clickdelete:'+this.state.deleteThis);
+    this.setState(Object.assign({}, this.state, { deleteThis: !this.state.deleteThis }));
+    console.log('clickdelete:' + this.state.deleteThis);
   }
-  render(){
-    console.log(this.props.state+"aaaa");
-    return (<div className="myhead">
-   {/* <span onClick={()=>{this.handleClickAll()}} className={this.state.selectAll?"checked":"unchecked"}>全选</span>*/}{/*这里的绑定用箭头函数()=>{this.handleClickAll()}，而用this.handleClickAll.bind(this),提示bind is undefined？？？后来全都换成箭头函数，就不出错，然后又全都换成bind又都正常，不报错了。诡异！*/}
-   {/* <span onClick={this.handleClickDelete.bind(this)} className={this.state.deleteThis?"checked":"unchecked"}>删除</span> 没使用redux时使用的方法*/}
-    <span onClick={()=>{this.props.dispatch({type:"CHANGE_SELECT",payload:"all"})}} className={this.props.state.selectAll?"checked":"unchecked"}>全选</span>
-    <span onClick={()=>{this.props.dispatch({type:"CHANGE_DELETE",payload:"delete"})}} className={this.props.state.deleteThis?"checked":"unchecked"}>删除</span>
-    </div>);
-  }
+  
+render(){
+  // console.log(this.props.state+"aaaa");
+  //return (<div className="myhead">
+     //<span onClick={()=>{this.handleClickAll()}} className={this.state.selectAll?"checked":"unchecked"}>全选</span>{/*没使用redux时使用的方法.这里的绑定用箭头函数()=>{this.handleClickAll()}，而用this.handleClickAll.bind(this),提示bind is undefined？？？后来全都换成箭头函数，就不出错，然后又全都换成bind又都正常，不报错了。诡异！*/}
+     //<span onClick={this.handleClickDelete.bind(this)} className={this.state.deleteThis?"checked":"unchecked"}>删除</span> {/*没使用redux时使用的方法*/}
+  //</div>);
+  //return (<div className="myhead">
+    // <span onClick={() => { this.props.dispatch({ type: "CHANGE_SELECT", payload: "all" }) }} className={this.props.state.selectAll ? "checked" : "unchecked"}>全选</span>{/*使用redux,但没有使用react-redux时使用的方法*/}
+   // <span onClick={() => { this.props.dispatch({ type: "CHANGE_DELETE", payload: "delete" }) }} className={this.props.state.deleteThis ? "checked" : "unchecked"}>删除</span>
+  //</div>);
+  console.log(this.props.state1);
+  return (<div className="myhead">
+     <span onClick={() => { this.props.dispatch1({ type: "CHANGE_SELECT", payload: "all" }) }} className={this.props.state1.selectAll ? "checked" : "unchecked"}>全选</span>{/*使用redux,并且使用react-redux时使用的方法*/}
+    <span onClick={() => { this.props.dispatch1({ type: "CHANGE_DELETE", payload: "delete" }) }} className={this.props.state1.deleteThis ? "checked" : "unchecked"}>删除</span>
+  </div>);
+  
 }
+}
+
+function mapStateToProps(state) {//redux(使用 react-redux)
+      return {
+          state1: state
+          }
+        }
+  function mapDispatchToProps(dispatch) {//redux(使用 react-redux)
+      return {
+        dispatch1: dispatch
+      }
+    }
+
+  export const Headwrap = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Head);
--- containers/index.jsx
+++ containers/index.jsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { createStore } from 'redux';
-import { Head } from './head';//这里Head要加上大括号，否则报类型错误。请看es6的语法
+import { connect } from 'react-redux';
+import { Provider } from 'react-redux';
+import { Headwrap } from './head';//这里Head要加上大括号，否则报类型错误。请看es6的语法
 import { Content } from './content';
 
 
@@ -12,15 +14,17 @@ const reducer = (state = { selectAll: false, deleteThis: false }, action) => {
   switch (action.type) {
     case "CHANGE_SELECT":
       console.log(action.payload);
-      console.log(action);
-      state.selectAll = !state.selectAll;
-      return state;
+      // console.log(action);
+      // state.selectAll = !state.selectAll;
+      console.log(state);
+      return Object.assign({}, state,{selectAll:!state.selectAll});
       break;
     case "CHANGE_DELETE":
       console.log(action.payload);
-      console.log(action);
-      state.deleteThis = !state.deleteThis;
-      return state;
+      // console.log(action);
+      // state.deleteThis = !state.deleteThis;//不要直接修改state，要使用下面的Object.assign方法
+      // return state;//不要直接修改state，要使用下面的Object.assign方法
+      return Object.assign({}, state,{deleteThis:!state.deleteThis});
       break;
     default:
       console.log(action);
@@ -37,19 +41,32 @@ class Wrap extends React.Component {
         this.state={
         };
     }//使用redux后，状态全部在store中管理，这里就不用state了。
-  render() {
-    console.log(this.props.state);
+  /*render() {
     return (<div>
       <Head dispatch={this.props.dispatch} state={this.props.state} />
       <Content />
     </div>);
+  }*///redux(不使用 react-redux)
+  render() {//redux(且使用 react-redux)
+    return (<div>
+      <Headwrap/>
+      <Content />
+    </div>);
   }
 }
-const watchRender = () => {
-  ReactDOM.render(
-    <Wrap dispatch={store.dispatch} state={store.getState()} />,
+
+
+// const watchRender = () => {//redux(不使用 react-redux)
+  // ReactDOM.render(
+  //   <Wrap dispatch={store.dispatch} state={store.getState()} />,
+  //   document.getElementById('container')
+  // );
+// }//redux(如使用 react-redux)
+ReactDOM.render(//redux(且使用 react-redux)
+  (<Provider store={store}>
+    <Wrap/>
+  </Provider>),
     document.getElementById('container')
   );
-}
-watchRender();
-store.subscribe(watchRender);//注意，通过reducer来更新state后，不会自动重新渲染页面，要通过store.subscribe方法来订阅实时渲染
+// watchRender();//redux(不使用 react-redux)
+// store.subscribe(watchRender);//redux(不使用 react-redux)//注意，通过reducer来更新state后，不会自动重新渲染页面，要通过store.subscribe方法来订阅实时渲染
