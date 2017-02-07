2e5f9c1c7a6bb72bf38f5261bd21701e30187e32
wenqian <wenqian@123.com>
2017-02-07 19:00:25 +0800

    redux

--- containers/head.jsx
+++ containers/head.jsx
@@ -26,21 +26,24 @@ export class Head extends React.Component {
       selectAll: false,
       deleteThis: false
     };
-  }
-  handleClickAll(){
+  }//用redux后，状态都写在了store中，这里就不用state了。
+  handleClickAll(){//没有用redux时使用的方法
     // this.state.selectAll=!this.state.selectAll;//不能用这种方法，要用下面的setState来更改状态
     this.setState(Object.assign({},this.state,{selectAll:!this.state.selectAll}));
     console.log('clickAll:'+this.state.selectAll);
   }
-  handleClickDelete(){
+  handleClickDelete(){//没有用redux时使用的方法
     // this.state.deleteThis=!this.state.deleteThis;
     this.setState(Object.assign({},this.state,{deleteThis:!this.state.deleteThis}));
     console.log('clickdelete:'+this.state.deleteThis);
   }
   render(){
+    console.log(this.props.state+"aaaa");
     return (<div className="myhead">
-    <span onClick={()=>{this.handleClickAll()}} className={this.state.selectAll?"checked":"unchecked"}>全选</span>{/*这里的绑定用箭头函数()=>{this.handleClickAll()}，而用this.handleClickAll.bind(this),提示bind is undefined？？？后来全都换成箭头函数，就不出错，然后又全都换成bind又都正常，不报错了。诡异！*/}
-    <span onClick={this.handleClickDelete.bind(this)} className={this.state.deleteThis?"checked":"unchecked"}>删除</span>
+   {/* <span onClick={()=>{this.handleClickAll()}} className={this.state.selectAll?"checked":"unchecked"}>全选</span>*/}{/*这里的绑定用箭头函数()=>{this.handleClickAll()}，而用this.handleClickAll.bind(this),提示bind is undefined？？？后来全都换成箭头函数，就不出错，然后又全都换成bind又都正常，不报错了。诡异！*/}
+   {/* <span onClick={this.handleClickDelete.bind(this)} className={this.state.deleteThis?"checked":"unchecked"}>删除</span> 没使用redux时使用的方法*/}
+    <span onClick={()=>{this.props.dispatch({type:"CHANGE_SELECT",payload:"all"})}} className={this.props.state.selectAll?"checked":"unchecked"}>全选</span>
+    <span onClick={()=>{this.props.dispatch({type:"CHANGE_DELETE",payload:"delete"})}} className={this.props.state.deleteThis?"checked":"unchecked"}>删除</span>
     </div>);
   }
 }
--- containers/index.jsx
+++ containers/index.jsx
@@ -1,24 +1,55 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import {Head} from './head';//这里Head要加上大括号，否则报类型错误。请看es6的语法
-import {Content} from './content';
+import { createStore } from 'redux';
+import { Head } from './head';//这里Head要加上大括号，否则报类型错误。请看es6的语法
+import { Content } from './content';
 
-require('./index.scss');
+
+require('./index.scss');//要进行分离的css文件
+
+
+const reducer = (state = { selectAll: false, deleteThis: false }, action) => {
+  switch (action.type) {
+    case "CHANGE_SELECT":
+      console.log(action.payload);
+      console.log(action);
+      state.selectAll = !state.selectAll;
+      return state;
+      break;
+    case "CHANGE_DELETE":
+      console.log(action.payload);
+      console.log(action);
+      state.deleteThis = !state.deleteThis;
+      return state;
+      break;
+    default:
+      console.log(action);
+      return state;
+      break;
+  }
+}
+const store = createStore(reducer);//store一创建就会执行一次reducer，以便得到初始的state，所以记得在reducer中返回state
+// console.log(store.getState());之前忘记在reducer的default语句中return state，导致初始化时store.getState为undefined
 // import {Bottom} from './bottom';
-class Wrap extends React.Component{
+class Wrap extends React.Component {
   constructor() {
         super();
         this.state={
         };
-    }
+    }//使用redux后，状态全部在store中管理，这里就不用state了。
   render() {
+    console.log(this.props.state);
     return (<div>
-      <Head/>
-      <Content/>
+      <Head dispatch={this.props.dispatch} state={this.props.state} />
+      <Content />
     </div>);
   }
 }
-ReactDOM.render(
-  <Wrap/>,
-  document.getElementById('container')
-);
+const watchRender = () => {
+  ReactDOM.render(
+    <Wrap dispatch={store.dispatch} state={store.getState()} />,
+    document.getElementById('container')
+  );
+}
+watchRender();
+store.subscribe(watchRender);//注意，通过reducer来更新state后，不会自动重新渲染页面，要通过store.subscribe方法来订阅实时渲染
