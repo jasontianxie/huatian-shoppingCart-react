600a219578d5042439117e6c3d78dc5ceb2054f1
wenqian <wenqian@123.com>
2017-02-10 18:28:47 +0800

    121212

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # huatian-shoppingCart-react
 画甜购物车页面，用react制作，用于学习
+加入了纯redux
+后面又加入了react-redux
--- containers/index.jsx
+++ containers/index.jsx
@@ -3,7 +3,7 @@ import * as ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { connect } from 'react-redux';
 import { Provider } from 'react-redux';
-import { Headwrap } from './head';//这里Head要加上大括号，否则报类型错误。请看es6的语法
+import { Headwrap } from './head';//这里Headwrap要加上大括号，否则报类型错误。请看es6的语法
 import { Content } from './content';
 
 
@@ -18,18 +18,18 @@ const reducer = (state = { selectAll: false, deleteThis: false }, action) => {
       // state.selectAll = !state.selectAll;
       console.log(state);
       return Object.assign({}, state,{selectAll:!state.selectAll});
-      break;
+      // break;
     case "CHANGE_DELETE":
       console.log(action.payload);
       // console.log(action);
       // state.deleteThis = !state.deleteThis;//不要直接修改state，要使用下面的Object.assign方法
       // return state;//不要直接修改state，要使用下面的Object.assign方法
       return Object.assign({}, state,{deleteThis:!state.deleteThis});
-      break;
+      // break;
     default:
       console.log(action);
       return state;
-      break;
+      // break;
   }
 }
 const store = createStore(reducer);//store一创建就会执行一次reducer，以便得到初始的state，所以记得在reducer中返回state
